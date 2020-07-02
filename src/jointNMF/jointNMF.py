097e9592783f5c7b5bd47bb0023e0e2ef27bf5c9
Karthik <karthikjagadeesh@gmail.com>
2020-07-02 07:35:39 +0000

    making matrix multiplications and dot products sparse

--- src/jointNMF/jointNMF.py
+++ src/jointNMF/jointNMF.py
@@ -1,18 +1,23 @@
 import numpy as np
+import scipy
+
+from scipy import sparse
 from time import time
 from sklearn.utils import check_random_state
 from sklearn.decomposition import NMF
+from sklearn.utils.extmath import safe_sparse_dot
 
 # constants
 _largenumber = 1E100
 _smallnumber = 1E-06
 _smallnumber2 = 1E-04
 
+
 class JointNMF:
     def __init__(self, Xh, Xd, Wh=None, Hh=None, Wshh=None, Hshh=None, Wshd=None, Hshd=None, Wd=None, Hd=None, 
                  nh_components=10, nsh_components=10, nd_components=10, gamma=1, mu=None):
-        self.Xh = np.copy(Xh)
-        self.Xd = np.copy(Xd)
+        self.Xh = scipy.sparse.csr_matrix(Xh).copy()
+        self.Xd = scipy.sparse.csr_matrix(Xd).copy()
 
         # initializations
         self.nh_components = nh_components
@@ -31,14 +36,14 @@ class JointNMF:
         
         # healthy programs
         if Wh is None:
-            self.Wh = nmfh.fit_transform(Xh)
+            self.Wh = scipy.sparse.csr_matrix(nmfh.fit_transform(Xh))
         else:
             if (Wh.shape != (self.Xh.shape[0], self.nh_components + self.nsh_components)):
                 raise ValueError("Initial Wh has wrong shape.")
             self.Wh = np.copy(Wh)
             
         if Hh is None:
-            self.Hh = nmfh.components_
+            self.Hh = scipy.sparse.csr_matrix(nmfh.components_)
         else:
             if (Hh.shape != (self.nh_components + self.nsh_components, self.Xh.shape[1])):
                 raise ValueError("Initial Wh has wrong shape.")
@@ -46,14 +51,14 @@ class JointNMF:
         
         # disease programs
         if Wd is None:
-            self.Wd = nmfd.fit_transform(Xd)
+            self.Wd = scipy.sparse.csr_matrix(nmfd.fit_transform(Xd))
         else:
             if (Wd.shape != (self.Xd.shape[0], self.nd_components + self.nsh_components)):
                 raise ValueError("Initial Wd has wrong shape.")
             self.Wd = np.copy(Wd)
         
         if Hd is None:
-            self.Hd = nmfd.components_
+            self.Hd = scipy.sparse.csr_matrix(nmfd.components_)
         else:
             if (Hd.shape != (self.nd_components + self.nsh_components, self.Xd.shape[1])):
                 raise ValueError("Initial Wd has wrong shape.")
@@ -63,20 +68,22 @@ class JointNMF:
         if mu:
             self.mu = mu
         else:
-            healthy_diff = 0.5*np.linalg.norm(self.Xh - np.dot(self.Wh, self.Hh), ord='fro')**2
-            disease_diff = 0.5*np.linalg.norm(self.Xd - np.dot(self.Wd, self.Hd), ord='fro')**2
-            denominator = np.linalg.norm(self.Wh, ord='fro')**2 + np.linalg.norm(self.Wd, ord='fro')**2
+            healthy_diff = 0.5*sparse.linalg.norm(self.Xh - safe_sparse_dot(self.Wh, self.Hh), ord='fro')**2
+            disease_diff = 0.5*sparse.linalg.norm(self.Xd - safe_sparse_dot(self.Wd, self.Hd), ord='fro')**2
+            denominator = sparse.linalg.norm(self.Wh, ord='fro')**2 + sparse.linalg.norm(self.Wd, ord='fro')**2
             self.mu = (healthy_diff + disease_diff)/denominator
     
     @property
     def cost(self):
+        self.Wh = self.Wh.tocsr()
+        self.Wd = self.Wd.tocsr()
         self.Wshh = self.Wh[:,:self.nsh_components]
         self.Wshd = self.Wd[:,:self.nsh_components]
         
-        diff1 = 0.5*np.linalg.norm(self.Xh - np.dot(self.Wh, self.Hh), ord='fro')**2
-        diff2 = 0.5*np.linalg.norm(self.Xd - np.dot(self.Wd, self.Hd), ord='fro')**2
-        diff3 = (self.mu/2)*np.linalg.norm(self.Wh, ord='fro')**2 + (self.mu/2)*np.linalg.norm(self.Wd, ord='fro')
-        diff4 = (self.gamma/2)*np.linalg.norm(self.Wshh-self.Wshd, ord='fro')**2
+        diff1 = 0.5*sparse.linalg.norm(self.Xh - safe_sparse_dot(self.Wh, self.Hh), ord='fro')**2
+        diff2 = 0.5*sparse.linalg.norm(self.Xd - safe_sparse_dot(self.Wd, self.Hd), ord='fro')**2
+        diff3 = (self.mu/2)*sparse.linalg.norm(self.Wh, ord='fro')**2 + (self.mu/2)*sparse.linalg.norm(self.Wd, ord='fro')
+        diff4 = (self.gamma/2)*sparse.linalg.norm(self.Wshh-self.Wshd, ord='fro')**2
         chi2 = diff1 + diff2 + diff3 + diff4
         return chi2
     
@@ -88,37 +95,37 @@ class JointNMF:
         niter = 0
         chi2 = self.cost
         oldchi2 = _largenumber
-        
-        while (niter < self.maxiters) and np.abs((oldchi2-chi2)/chi2) > self.tol: #((oldchi2-chi2)/chi2 > self.tol):
+        maxiters = maxiters if maxiters else self.maxiters
+        while (niter < maxiters) and np.abs((oldchi2-chi2)/chi2) > self.tol: #((oldchi2-chi2)/chi2 > self.tol):
             # update Wh
             scale2=np.append((self.gamma+self.mu)*np.ones(self.nsh_components), (self.mu)*np.ones(self.nh_components))
             self.Wshd = self.Wd[:,:self.nsh_components]
-            Wh_up1 = np.dot(self.Xh, self.Hh.T)
-            Wshd_transform = self.Wshd * (self.gamma*np.ones((self.Xh.shape[0], self.nsh_components)))
-            zeros = np.zeros((self.Xh.shape[0], self.nh_components))
-            Wh_up2 = np.hstack((Wshd_transform, zeros)) + _smallnumber2
-            Wh_down = np.dot(self.Wh, np.dot(self.Hh, self.Hh.T)) + np.dot(self.Wh, np.diag(scale2)) + _smallnumber2
-            self.Wh = self.Wh*((Wh_up1 + Wh_up2)/Wh_down)
+            Wh_up1 = safe_sparse_dot(self.Xh, self.Hh.T)
+            Wshd_transform = self.Wshd.multiply(scipy.sparse.csr_matrix(self.gamma*np.ones((self.Xh.shape[0], self.nsh_components))))
+            zeros = scipy.sparse.csr_matrix(np.zeros((self.Xh.shape[0], self.nh_components)))
+            Wh_up2 = scipy.sparse.hstack((Wshd_transform, zeros)) #+ _smallnumber2
+            Wh_down = safe_sparse_dot(self.Wh, safe_sparse_dot(self.Hh, self.Hh.T)) + safe_sparse_dot(self.Wh, np.diag(scale2)) #+ _smallnumber2
+            self.Wh = self.Wh.multiply((Wh_up1 + Wh_up2)/Wh_down).tocsr()
             
             # update Hh
-            Hh_up = np.dot(self.Wh.T, self.Xh) + _smallnumber2
-            Hh_down = np.dot(np.dot(self.Wh.T, self.Wh), self.Hh) + _smallnumber2
-            self.Hh = self.Hh*(Hh_up/Hh_down)
+            Hh_up = safe_sparse_dot(self.Wh.T, self.Xh) #+ _smallnumber2
+            Hh_down = safe_sparse_dot(safe_sparse_dot(self.Wh.T, self.Wh), self.Hh) #+ _smallnumber2
+            self.Hh = self.Hh.multiply(Hh_up/Hh_down).tocsr()
             
             # update Wd
             scale2=np.append((self.gamma+self.mu)*np.ones(self.nsh_components), self.mu*np.ones(self.nd_components))
             self.Wshh = self.Wh[:,:self.nsh_components]
-            Wd_up1 = np.dot(self.Xd, self.Hd.T)
-            Wshh_transform = self.Wshh * (self.gamma*np.ones((self.Xd.shape[0], self.nsh_components)))
+            Wd_up1 = safe_sparse_dot(self.Xd, self.Hd.T)
+            Wshh_transform = self.Wshh.multiply(scipy.sparse.csr_matrix(self.gamma*np.ones((self.Xd.shape[0], self.nsh_components))))
             zeros = np.zeros((self.Xd.shape[0], self.nd_components))
-            Wd_up2 = np.hstack((Wshh_transform, zeros)) + _smallnumber2
-            Wd_down = np.dot(self.Wd, np.dot(self.Hd, self.Hd.T)) + np.dot(self.Wd, np.diag(scale2)) + _smallnumber2 
-            self.Wd = self.Wd*((Wd_up1 + Wd_up2)/Wd_down)
+            Wd_up2 = scipy.sparse.hstack((Wshh_transform, zeros)) #+ _smallnumber2
+            Wd_down = safe_sparse_dot(self.Wd, safe_sparse_dot(self.Hd, self.Hd.T)) + safe_sparse_dot(self.Wd, np.diag(scale2)) #+ _smallnumber2 
+            self.Wd = self.Wd.multiply((Wd_up1 + Wd_up2)/Wd_down).tocsr()
                         
             # update Hd
-            Hd_up = np.dot(self.Wd.T, self.Xd) + _smallnumber2
-            Hd_down = np.dot(np.dot(self.Wd.T, self.Wd), self.Hd) + _smallnumber2
-            self.Hd = self.Hd*(Hd_up/Hd_down)
+            Hd_up = safe_sparse_dot(self.Wd.T, self.Xd) #+ _smallnumber2
+            Hd_down = safe_sparse_dot(safe_sparse_dot(self.Wd.T, self.Wd), self.Hd) #+ _smallnumber2
+            self.Hd = self.Hd.multiply(Hd_up/Hd_down).tocsr()
                         
             # chi2
             oldchi2 = chi2
